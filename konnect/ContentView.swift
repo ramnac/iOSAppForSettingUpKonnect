8db74d60488126f3c33e81ab986b3da61ff2d9b1
Ram <ramunachu7@gmail.com>
2019-07-19 23:41:17 +1200

    initial commit of full repository

---
+++ konnect/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  konnect
+//
+//  Created by Ram on 19/07/19.
+//  Copyright © 2019 andersenev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
